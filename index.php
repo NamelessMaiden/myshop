2347e39d70f2df1e37d3da772dee872fbd78a782
NamelessMaiden <68120476+NamelessMaiden@users.noreply.github.com>
2021-02-09 16:19:52 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<html>
+
+<head>
+    <title>Мой магазин</title>
+    <style>
+        .myDiv {
+            background-color: #acee90;
+        }
+    </style>
+</head>
+<body>
+
+<h1>
+
+    <img src="img\myshoplogo.png" width="800" height="300">
+
+</h1>
+
+<h2>Мой супер товар</h2>
+
+<div class="myDiv">
+    <br>
+    <p>   Характеристики:</p>
+        <h3>
+            <ul>
+                <li>Цвет: красный  </li>
+                <li>Диагональ: 9см </li>
+                <li>Высота: 15см   </li>
+            </ul>
+        </h3>
+
+        <label for="fname">Укажите количество товаров:</label>
+        <input type="text">
+        <input type="submit" value="Купить">
+    </br>
+    <br>
+    <br>
+
+</div>
+</body>
+
+</html>
+
