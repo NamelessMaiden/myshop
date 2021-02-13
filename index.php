1a16d3dab338b8842b1669fc3cdd88e6392176f1
NamelessMaiden <68120476+NamelessMaiden@users.noreply.github.com>
2021-02-13 02:20:16 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,43 +1,48 @@
 <html>
-
 <head>
-    <title>Мой магазин</title>
-    <style>
-        .myDiv {
-            background-color: #acee90;
-        }
-    </style>
+    <meta charset="UTF-8"/>
+    <TITLE>myShop</TITLE>
+    <link href="styles/styles.css" rel="stylesheet" type="text/css"/>
 </head>
 <body>
+<div class="mainImage">
+    <div class="mainImage.bottom">
+        <div class="mainImage.bottom.text">
+            Buy now
+        </div>
+    </div>
 
-<h1>
-
-    <img src="img\myshoplogo.png" width="800" height="300">
-
-</h1>
-
-<h2>Мой супер товар</h2>
-
-<div class="myDiv">
-    <br>
-    <p>   Характеристики:</p>
-        <h3>
-            <ul>
-                <li>Цвет: красный  </li>
-                <li>Диагональ: 9см </li>
-                <li>Высота: 15см   </li>
-            </ul>
-        </h3>
-
-        <label for="fname">Укажите количество товаров:</label>
-        <input type="text">
-        <input type="submit" value="Купить">
-    </br>
-    <br>
-    <br>
+    <div class="mainImage.text">
+        Super awesome discount only today
+    </div>
+    <img src="img/Rectangle%201.pn">
 
 </div>
-</body>
 
+<div class="mainDescription">
+
+    <div class="mainDescription.intro">
+       Why us?
+    </div>
+
+    <div class="mainDescription.text">
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+    when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+    It has survived not only five centuries, but also the leap into electronic typesetting,
+    remaining essentially unchanged.
+    It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+    and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+    </div>
+
+</div>
+
+<div class="productsDescription">
+    <p>Top products</p>
+</div>
+
+<div class="test">This div element has a top margin of 100px, a right margin of 150px, a bottom margin of 100px, and a left margin of 80px.</div>
+
+</body>
 </html>
 
