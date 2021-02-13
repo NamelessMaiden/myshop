3113fe2061270476bcc5e0f773166a4eb245ed1e
NamelessMaiden <68120476+NamelessMaiden@users.noreply.github.com>
2021-02-13 07:24:22 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,43 +1,347 @@
 <html>
-
 <head>
-    <title>Мой магазин</title>
-    <style>
-        .myDiv {
-            background-color: #acee90;
-        }
-    </style>
+    <meta charset="UTF-8"/>
+    <TITLE>myShop</TITLE>
+    <link href="styles/styles.css" rel="stylesheet" type="text/css"/>
 </head>
+<style>
+    .SuperAwesomeDiscount {
+        position: absolute;
+        top: 20px; left: 50px;
+        width: 207px;
+        height: 167px;
+
+        font-family: 'Roboto', sans-serif;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 36px;
+        line-height: 42px;
+    }
+    .whyUs {
+        position: absolute;
+
+        top: 330px; left: 380px;
+
+        font-family: 'Roboto', sans-serif;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 36px;
+        line-height: 42px;
+        text-align: center;
+    }
+    .OurBlogStories{
+        position: absolute;
+
+        top: 1050px; left: 310px;
+
+        font-family: 'Roboto', sans-serif;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 36px;
+        line-height: 42px;
+        text-align: center;
+    }
+    .description {
+
+        position: absolute;
+        top: 410px; left: 15px;
+
+        font-family: 'Roboto', sans-serif;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 14px;
+        line-height: 16px;
+        text-align: center;
+
+
+    }
+    .topProductsText {
+        position: absolute;
+        top: 485px; left: 350px;
+
+
+        font-family: 'Roboto', sans-serif;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 36px;
+        line-height: 42px;
+        text-align: center;
+    }
+    .sizeWord {
+        position: relative;
+        top: 40px; left: 20px;
+
+        font-family: 'Roboto', sans-serif;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 18px;
+        line-height: 16px;
+        display: flex;
+        align-items: center;
+    }
+    .containerSize {
+        position: relative;
+        top: -6px; left: 70px;
+        display: flex;
+
+    }
+    .containerSize > div {
+        font-family: 'Roboto', sans-serif;
+        background-color: #f1f1f1;
+        margin: 5px;
+        padding: 8px;
+        font-size: 12px;
+    }
+    .containerColor {
+        position: relative;
+        top: -56px; left: 70px;
+        display: flex;
+
+    }
+    .containerColor > div {
+        font-family: 'Roboto', sans-serif;
+        background-color: #f1f1f1;
+        margin: 5px;
+        padding: 8px;
+        font-size: 12px;
+    }
+    .colorWord {
+        position: relative;
+        top: -10px;
+        left: 20px;
+
+        font-family: 'Roboto', sans-serif;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 18px;
+        line-height: 16px;
+        display: flex;
+        align-items: center;
+    }
+    .textDescriptionProduct{
+        position: relative;
+        top: -40px;
+        left: 20px;
+
+        width: 217px;
+        height: 63.56px;
+
+        font-family: 'Roboto', sans-serif;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 14px;
+        line-height: 16px;
+        display: flex;
+        align-items: center;
+    }
+    .input1 {
+        display:block;
+        width:50%;
+        position: relative;
+        top: -30px;
+        left: 20px;
+    }
+    .BuyButtom{
+        position: absolute;
+        bottom: 10px;
+        left: 160px;
+
+        width: 85px;
+        height: 30px;
+
+        font-size: 14px;
+        background: #49AB58;
+        font-family: 'Roboto', sans-serif;
+        display: flex;
+        align-items: center;
+        border-radius: 15px;
+        text-align: center;
+
+    }
+    .rectangle1 {
+        position: absolute;
+        width: 878px;
+        height: 330px;
+    }
+    .rectangle2 {
+
+        position: absolute;
+        top: 250px; left: 40px;
+        width: 170px;
+        height: 53px;
+        text-align: center;
+        font-size: 24px;
+        font-family: 'Roboto', sans-serif;
+
+        background: #49AB58;
+        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+        border-radius: 15px;
+    }
+    .rectangle4 {
+        border: 1px solid #4CAF50;
+        position: relative;
+        top: 20px; left: 25px;
+        width: 198px;
+        height: 195px;
+
+    }
+    .rectangle5 {
+        top: 600px; left: 20px;
+        position: absolute;
+        width: 251px;
+        height: 449px;
+        border: 1px solid #4CAF50;
+    }
+    .rectangle5_2 {
+        top: 600px; left: 320px;
+        position: absolute;
+        width: 251px;
+        height: 449px;
+        border: 1px solid #4CAF50;
+    }
+    .rectangle5_3 {
+        top: 600px; left: 620px;
+        position: absolute;
+        width: 251px;
+        height: 449px;
+        border: 1px solid #4CAF50;
+</style>
+
+
+
 <body>
+<div class="banner">
+    <div class="rectangle1">
+        <img src="img/rectangle1.png"
+    </div>
+    <button class="rectangle2">Buy Now</button>
+    <div class="SuperAwesomeDiscount">
+        <p> Super awesome discount only today</p>
+    </div>
+    <div>
+        <div class="whyUs">
+            <p>Why us?</p>
+        </div>
 
-<h1>
+        <div class="description">
+            <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                It has survived not only five centuries, but also the leap into electronic typesetting,
+                remaining essentially unchanged.
+                It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+                and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+            </p>
+        </div>
+        <div class="OurBlogStories">
+            <p>Our blog stories</p>
+        </div>
 
-    <img src="img\myshoplogo.png" width="800" height="300">
 
-</h1>
+    </div>
+    <div>
+        <div class="topProductsText">
+            <p>Top Products</p>
+        </div>
+        </div>
+    <div class="rectangle5">
+        <div class="rectangle4">
+            <img src="img/rectangle4.png" alt="productImg">
+        </div>
+        <div class="sizeWord">
+            <p> Size:</p>
+        </div>
+        <div class="containerSize">
+            <div>XS</div>
+            <div>S</div>
+            <div>L</div>
+            <div>XL</div>
+        </div>
+        <div class="colorWord">
+            <p> Color:</p>
+        </div>
+        <div class="containerColor">
+            <div>Black</div>
+        </div>
+        <div class="textDescriptionProduct">
+            <p>
+                It is a long established fact that a reader will be
+                distracted by the readable content of a page when
+                looking at its layout
+            </p>
+        </div>
+        <div class="input1">
+            <input type="text" id="number">
+        </div>
+        <button class="BuyButtom">Add to cart</button>
+    </div>
+    <div class="rectangle5_2">
+        <div class="rectangle4">
+            <img src="img/rectangle4.png" alt="productImg">
+        </div>
+        <div class="sizeWord">
+            <p> Size:</p>
+        </div>
+        <div class="containerSize">
+            <div>XS</div>
+            <div>S</div>
+            <div>L</div>
+            <div>XL</div>
+        </div>
+        <div class="colorWord">
+            <p> Color:</p>
+        </div>
+        <div class="containerColor">
+            <div>Black</div>
+        </div>
+        <div class="textDescriptionProduct">
+            <p>
+                It is a long established fact that a reader will be
+                distracted by the readable content of a page when
+                looking at its layout
+            </p>
+        </div>
+        <div class="input1">
+            <input type="text" id="number">
+        </div>
+        <button class="BuyButtom">Add to cart</button>
+    </div>
+    <div class="rectangle5_3">
 
-<h2>Мой супер товар</h2>
-
-<div class="myDiv">
-    <br>
-    <p>   Характеристики:</p>
-        <h3>
-            <ul>
-                <li>Цвет: красный  </li>
-                <li>Диагональ: 9см </li>
-                <li>Высота: 15см   </li>
-            </ul>
-        </h3>
-
-        <label for="fname">Укажите количество товаров:</label>
-        <input type="text">
-        <input type="submit" value="Купить">
-    </br>
-    <br>
-    <br>
-
+            <div class="rectangle4">
+                <img src="img/rectangle4.png" alt="productImg">
+            </div>
+            <div class="sizeWord">
+                <p> Size:</p>
+            </div>
+            <div class="containerSize">
+                <div>XS</div>
+                <div>S</div>
+                <div>L</div>
+                <div>XL</div>
+            </div>
+            <div class="colorWord">
+                <p> Color:</p>
+            </div>
+            <div class="containerColor">
+                <div>Black</div>
+            </div>
+            <div class="textDescriptionProduct">
+                <p>
+                    It is a long established fact that a reader will be
+                    distracted by the readable content of a page when
+                    looking at its layout
+                </p>
+            </div>
+            <div class="input1">
+                <input type="text" id="number">
+            </div>
+            <button class="BuyButtom">Add to cart</button>
+        </div>
+    </div>
 </div>
-</body>
 
+</body>
 </html>
+
 
